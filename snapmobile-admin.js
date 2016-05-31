d14ff38abd48f7c5cdc9814f8012fcd6b9554618
cablej <jackhcable@gmail.com>
2016-05-31 09:05:44 -0500

    Removed sidebarItems function

--- snapmobile-admin.js
+++ snapmobile-admin.js
@@ -1,14 +1,3 @@
 "use strict";
 
 var admin = require('./dist/admin');
-
-module.exports = {
-	iconType: {
-		DASHBOARD : "dashboard",
-		USERS: "users",
-		MOBILE: "mobile"
-	},
-	sidebarItems: function(items) {
-		admin.sidebarItems(items);
-	}
-}
